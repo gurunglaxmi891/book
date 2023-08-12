a99b652f728257aa79fd6f6220d2587f36523a2f
Prakash4400 <taekwondo4400@gmail.com>
2023-08-12 08:49:27 +0545

    responsive sign up form

--- partials/sign_log_in_form.php
+++ partials/sign_log_in_form.php
@@ -10,27 +10,30 @@
     
 
     <section id="form">
-               <h1>Read Recycle</h1>
-         
-                <div class="form-container">
-                    <h2>Create a new account</h2>
-                    <form action="">
-                        <input type="text" placeholder="Enter Your Full Name">
-                        <br><br>
-        
-                        <input type="email" placeholder="E-mail">
-                        <br><br>
-    
-                        <input type="password" placeholder="Enter strong password">
-                        <br><br>
-    
-                    </form>
+        <div class="container flex-style">
+            <h1>Read Recycle</h1>
+      
+             <div class="form-container">
+                 <h2>Create a new account</h2>
+                 <form action="">
+                     <input type="text" placeholder="Enter Your Full Name">
+                     <br><br>
+     
+                     <input type="email" placeholder="E-mail">
+                     <br><br>
+ 
+                     <input type="password" placeholder="Enter strong password">
+                     <br><br>
+ 
+                 </form>
 
-                    <a href="" class="btn">Sign Up</a>
+                 <a href="" class="btn">Sign Up</a>
 
-                    <h3>Already have an account?</h3>
-                    <a href="" class="btn">Login</a>
-                </div>
+                 <h3>Already have an account?</h3>
+                 <a href="" class="btn">Login</a>
+             </div>
+
+        </div>
           
     </section>
 
